e355bbbdc6e1328e947c4844138f1c30c407bbb0
irinababunova <xleyla@gmail.com>
2023-10-28 22:33:42 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        final int applesBuratino = 23;
+        System.out.println("Количество яблок у Буратино - " + applesBuratino);
+
+        final int applesBarabas = 13;
+        System.out.println("Количество яблок, которые хочет отобрать Барабас - " + applesBarabas);
+
+        final int remainingApples = applesBuratino - applesBarabas;
+        System.out.println("У Буратино осталось - " + remainingApples + " яблок");
+    }
+}
